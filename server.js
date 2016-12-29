1f7e4719601464abb190f06a46f91bd8c6a9c893
kz4 <zkcneu@gmail.com>
2016-12-29 01:04:48 -0500

    application not working on openshift,  commented out chat server part to see if it's related to this

--- server.js
+++ server.js
@@ -12,29 +12,29 @@ var sockjs = require('sockjs');
 
 var connections = [];
 
-var chat = sockjs.createServer();
-chat.on('connection', function(conn) {
-    connections.push(conn);
-    var number = connections.length;
-    conn.write("Welcome, User " + number);
-    conn.on('data', function(message) {
-
-        // TODO: database insert/update/select
-
-        for (var ii=0; ii < connections.length; ii++) {
-            connections[ii].write("User " + number + " says: " + message);
-        }
-    });
-    conn.on('close', function() {
-        for (var ii=0; ii < connections.length; ii++) {
-            connections[ii].write("User " + number + " has disconnected");
-        }
-    });
-});
-
-var server = http.createServer();
-chat.installHandlers(server, {prefix:'/chat'});
-server.listen(9999, '127.0.0.1');
+// var chat = sockjs.createServer();
+// chat.on('connection', function(conn) {
+//     connections.push(conn);
+//     var number = connections.length;
+//     conn.write("Welcome, User " + number);
+//     conn.on('data', function(message) {
+//
+//         // TODO: database insert/update/select
+//
+//         for (var ii=0; ii < connections.length; ii++) {
+//             connections[ii].write("User " + number + " says: " + message);
+//         }
+//     });
+//     conn.on('close', function() {
+//         for (var ii=0; ii < connections.length; ii++) {
+//             connections[ii].write("User " + number + " has disconnected");
+//         }
+//     });
+// });
+//
+// var server = http.createServer();
+// chat.installHandlers(server, {prefix:'/chat'});
+// server.listen(9999, '127.0.0.1');
 
 
 app.use(bodyParser.json());

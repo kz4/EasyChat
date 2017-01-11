418a5bdadd377ad3f0896421d9c94d62971ea0db
kz4 <zkcneu@gmail.com>
2017-01-11 00:58:07 -0500

    Added login page and controller (only UI)

--- public/config.js
+++ public/config.js
@@ -6,6 +6,11 @@
     function Config($routeProvider) {
         $routeProvider
             .when("/", {
+                templateUrl: "views/user/login.view.client.html",
+                controller: "LoginController",
+                controllerAs: "model"
+            })
+            .when("/chat", {
                 templateUrl: "views/chat/chat.view.client.html",
                 controller: "ChatController",
                 controllerAs: "model"
